374777f799627d917af157ef9c12860429e71525
Sorokina-Alina <alina9551@icloud.com>
2024-02-22 19:00:20 +0300

    create questionPage

---
+++ components/QuestionPage.jsx
@@ -0,0 +1,18 @@
+// const React = require('react');
+// const Layout = require('./Layout');
+
+// function QuestionPage({ title, question }) {
+//   return (
+//     <Layout title={title}>
+//       <h2>{question.textQuestion}</h2>
+//       <img src={question.img} alt='' />
+//       <form className='send-form' action='' method='POST'>
+//         <input type='text' name='answer' placeholder='Введи ответ' />
+//         <button>Отправить</button>
+
+//       </form>
+//     </Layout>
+//   );
+// }
+
+// module.exports = QuestionPage;
--- routes/index.router.js
+++ routes/index.router.js
@@ -1,9 +1,15 @@
 const router = require('express').Router();
 
-const authViewRouter = require('./views/auth.view.router')
-const authApiRouter = require('./api/auth.api.router')
+const authViewRouter = require('./views/auth.view.router');
+const authApiRouter = require('./api/auth.api.router');
 
-router.use('/auth', authApiRouter)
-router.use('/auth', authViewRouter)
+const questionViewRouter = require('./views/question.view.router');
+// const questionApiRouter = require('./api/question.api.router');
+
+router.use('/auth', authApiRouter);
+router.use('/auth', authViewRouter);
+
+router.use('/question', questionViewRouter);
+// router.use('/question', questionApiRouter);
 
 module.exports = router;
