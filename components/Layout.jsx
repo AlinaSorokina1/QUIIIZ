427c0ec23c5a42fe76b0b169c8164c5bb9666568
Fresh-B1 <ggarzumanov@gmail.com>
2024-02-22 19:00:37 +0300

    created Main branch

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -2,11 +2,16 @@ const React = require('react');
 
 function Layout({ title, children }) {
   return (
-    <html lang='en'>
+    <html lang="en">
       <head>
         <title>{title}</title>
+        <link rel="stylesheet" href="/styles/index.css" />
       </head>
       <body>{children}</body>
+
+      <footer>
+        <span>🍕 Pizza Day АуФ!</span>
+      </footer>
     </html>
   );
 }
--- routes/index.router.js
+++ routes/index.router.js
@@ -1,3 +1,13 @@
 const router = require('express').Router();
 
+// views
+const mainRouter = require('./views/main.router');
+
+// api
+
+// route views
+router.use('/', mainRouter);
+
+// route api
+
 module.exports = router;
